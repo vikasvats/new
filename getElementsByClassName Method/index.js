bc6de5ca9f8d20f46ca8525682a96d1913a22f67
vikas chaudhary <124273440+vikasvats@users.noreply.github.com>
2026-01-08 09:04:19 +0530

    getElementsByClassName Method

---
+++ getElementsByClassName Method/index.js
@@ -0,0 +1,8 @@
+// Write your code below:
+const fruit = document.getElementsByClassName('fruit');
+// // console.log(fruit[1])
+fruit[2].style.backgroundColor = 'yellow'
+
+for (let i = 0; i < fruit.length; i++){
+    fruit[i].style.fontWeight = 'bold'
+}
